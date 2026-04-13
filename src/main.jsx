d5a7ce840418dc3d1f69ddff9a4425b8de0f47a9
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-04-13 21:49:11 +0100

    routing setup with react-router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
 
+/* scaffold original
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+*/
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
