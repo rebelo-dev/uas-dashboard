2e987fe680cfa1656591d0d433c028fec70d2a6d
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-04-15 07:06:13 +0100

    fix:dev environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
-const BASE_URL = import.meta.env.DEV
-    ? "http://localhost:3000"
-    : import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_BASE_DEV_URL;
+
 
 export const api = {
 
