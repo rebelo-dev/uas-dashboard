e3e78bd91bca5c600452a21407d55ac5462c669f
rebelo-dev <joao.rebelo.dev@gmail.com>
2026-04-19 21:29:24 +0100

    use state and use effect for  local testing
    making a loading screen while render builds

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,38 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "@/pages/Dashboard";
 import DronePage from "@/pages/DronePage";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loadingScreen";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+  }, []);
+
+  if (loading) {
+
+
+    return (
+      <>
+        <LoadingScreen />
+        <button onClick={() => setLoading(false)}> tornar loading false</button>
+      </>);
+
+
+  }
+
+
+
+
   return (
+
+
+
+
     <div className="min-h-screen bg-background text-foreground">
       <Routes>
         <Route path="/" element={<Dashboard />} />
@@ -14,6 +43,11 @@ export default function App() {
   );
 }
 /*
+   <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/drone/:id" element={<DronePage />} />
+      </Routes>
+
 
 TEST TO SEE IF TAILWIND IS WORKING
 
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+
+export default function LoadingScreen() {
+
+    return (<div>LOADINGSCREENCOMPONENT LINE</div>
+
+    );
+}
+
